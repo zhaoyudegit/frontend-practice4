96ab69c151c06772cfa7d8893900a58ff5d65320
zhaoyudegit <2025557585@qq.com>
2026-09-11 21:13:38 +0800

    添加数据

---
+++ script.js
@@ -0,0 +1,7 @@
+const courseList = [
+    { name: "高等数学", score: 82, credit: 4 },
+    { name: "大学物理", score: 75, credit: 3 },
+    { name: "英语", score: null, credit: 2 },
+    { name: "计算机基础", score: 90, credit: 2 },
+    { name: "思政", score: "abc", credit: 3 },
+];
